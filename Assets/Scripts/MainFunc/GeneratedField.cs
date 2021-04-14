b6e72b30d611d8eb39f2c3bdba802d153a86ac6b
Dio Kowalski <58032622+KowalskiVevo@users.noreply.github.com>
2021-04-15 00:15:00 +0500

    first commit

---
+++ Assets/Scripts/MainFunc/GeneratedField.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratedField : MonoBehaviour
+{
+    [SerialkzeField]
+    private int X,Y; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
